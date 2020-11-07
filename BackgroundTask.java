d4194a2110780e9a5d1d951d4dd81fb1b2ff7752
Sunny Gupta <vknow360@gmail.com>
2020-11-07 19:22:33 +0530

    Update BackgroundTask.java

--- BackgroundTask.java
+++ BackgroundTask.java
@@ -1,19 +1,23 @@
 package com.sunny.BackgroundTask;
+//package com.google.appinventor.components.runtime;
+
 import android.app.Activity;
-import android.app.IntentService;
-import android.app.Service;
+import android.app.job.JobParameters;
+import android.app.job.JobService;
 import android.content.Context;
 import android.content.Intent;
-import android.os.IBinder;
 import android.widget.Toast;
 import androidx.core.app.NotificationCompat;
 import androidx.core.app.NotificationManagerCompat;
-import com.google.appinventor.components.annotations.*;
-import com.google.appinventor.components.annotations.androidmanifest.*;
+import com.google.appinventor.components.annotations.DesignerComponent;
+import com.google.appinventor.components.annotations.SimpleFunction;
+import com.google.appinventor.components.annotations.SimpleObject;
+import com.google.appinventor.components.annotations.UsesServices;
+import com.google.appinventor.components.annotations.androidmanifest.ServiceElement;
 import com.google.appinventor.components.common.ComponentCategory;
-import com.google.appinventor.components.runtime.*;
+
 @DesignerComponent(version = 1,
-        description = "An example extension for calling procedures in a background service",
+        description = "An extension to run tasks in background",
         iconName = "https://res.cloudinary.com/andromedaviewflyvipul/image/upload/c_scale,h_20,w_20/v1571472765/ktvu4bapylsvnykoyhdm.png",
         nonVisible = true,
         category = ComponentCategory.EXTENSION)
@@ -28,68 +32,108 @@ public class BackgroundTask extends AndroidNonvisibleComponent{
         context = activity;
     }
     @SimpleFunction()
-    public void StartBackgroundService(String procedureName){
-        try {
-            context.startService(new Intent(context,BgService.class)
-            .putExtra("PROCEDURE_NAME",procedureName));
-        } catch (Exception e) {
-            e.printStackTrace();
-            Toast.makeText(context,e.getMessage(),Toast.LENGTH_LONG);
-        }
+    public void CreateComponent(int id,String componentName){
+        
     }
     @SimpleFunction()
     public void StopService(){
-        try {
-            context.stopService(new Intent(context,BgService.class));
-        } catch (Exception e) {
-            e.printStackTrace();
-            Toast.makeText(context,e.getMessage(),Toast.LENGTH_LONG);
-        }
+
     }
-    public static class BgService extends Service {
-        public Context mContext;
+    public static class BgService extends JobService {
+        public AActivity activity;
+        public CContainer container;
+        public FForm form;
         public NotificationManagerCompat notificationManager;
         public BgService(){
-            super();
+            activity = new AActivity(this);
+            form = new FForm(this);
+            container = new CContainer(this);
         }
 
         @Override
-        public IBinder onBind(Intent intent) {
-            return null;
+        public boolean onStartJob(JobParameters jobParameters) {
+            return true;
         }
 
         @Override
-        public void onCreate() {
-            super.onCreate();
-            mContext = getApplicationContext();
-            notificationManager = NotificationManagerCompat.from(mContext);
+        public boolean onStopJob(JobParameters jobParameters) {
+            return true;
         }
 
         @Override
         public int onStartCommand(Intent intent, int flags, int startId) {
-            NotificationCompat.Builder builder = new NotificationCompat.Builder(mContext,mContext.getApplicationInfo().name);
-            //builder.setContentTitle(title);
-            builder.setContentText("Service started by user");
-            builder.setAutoCancel(false);
-            builder.setVisibility(1);
-            builder.setPriority(1);
-            notificationManager.notify(12345,builder.build());
-            //Toast.makeText(context, "Service started by user.", Toast.LENGTH_LONG).show();
-            return super.onStartCommand(intent, flags, startId);
+            return START_NOT_STICKY;
         }
 
         @Override
         public void onDestroy() {
-            //Toast.makeText(context, "Service stopped by user.", Toast.LENGTH_LONG).show();
-            //notificationManager.cancel(12345);
-            NotificationCompat.Builder builder = new NotificationCompat.Builder(mContext,mContext.getApplicationInfo().name);
-            //builder.setContentTitle(title);
-            builder.setContentText("Service stopped by user");
-            builder.setAutoCancel(false);
-            builder.setVisibility(1);
-            builder.setPriority(1);
-            notificationManager.notify(12345,builder.build());
             super.onDestroy();
         }
+        public void GotEvent(Component component,String eventName, Object[] args){
+
+        }
+    }
+    public static class CContainer implements ComponentContainer{
+        private final BgService bgService;
+        public CContainer(BgService bgService){
+            this.bgService = bgService;
+        }
+        @Override
+        public Activity $context() {
+            return bgService.activity;
+        }
+
+        @Override
+        public Form $form() {
+            return bgService.form;
+        }
+
+        @Override
+        public void $add(AndroidViewComponent component) {
+
+        }
+
+        @Override
+        public void setChildWidth(AndroidViewComponent component, int width) {
+
+        }
+
+        @Override
+        public void setChildHeight(AndroidViewComponent component, int height) {
+
+        }
+
+        @Override
+        public int Width() {
+            return 0;
+        }
+
+        @Override
+        public int Height() {
+            return 0;
+        }
+    }
+    public static class FForm extends Form{
+        private final BgService bgService;
+        public FForm(BgService bgService){
+            this.bgService = bgService;
+        }
+
+        @Override
+        public boolean dispatchEvent(Component component, String componentName, String eventName, Object[] args) {
+            bgService.GotEvent(component,eventName,args);
+            return true;
+        }
+
+        @Override
+        public void dispatchGenericEvent(Component component, String eventName, boolean notAlreadyHandled, Object[] args) {
+            bgService.GotEvent(component,eventName,args);
+        }
+    }
+    public static class AActivity extends Activity{
+        public AActivity(BgService bgService){}
+        /*public void attachToMain(Context context){
+            attachBaseContext(context);
+        }*/
     }
 }
